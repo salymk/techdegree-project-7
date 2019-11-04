9b9689f9d7b9f9858d10f3b59a12ce9a803ef8fa
salymk <khokumdarakhmedov@gmail.com>
2019-11-03 18:29:14 -0700

    Add files via upload
    Updated a whole bunch of stuff. I think it should meet expectations.

--- js/webapp.js
+++ js/webapp.js
@@ -1,16 +1,5 @@
-
 //Alert banner
 const alert = document.getElementById('alert');
-
-//Create the html for the banner
-// alert.innerHTML =
-// `
-//      <div class="alert-banner">
-//           <p><strong>Alert:</strong> You have <strong>6</strong> overdue
-//           tasks to complete</p>
-//           <p class="alert-banner-close">x</p>
-//       </div>
-//   ` ;
 
 //If the user clicks the close button, then
 //hide the alert banner
@@ -20,22 +9,6 @@ alert.addEventListener('click', e => {
     alert.style.display = 'none';
   }
 });
-
-
-// Chart widgets
-// let trafficCanvas = document.getElementById("traffic-chart").getContext('2d');
-//
-// let trafficData = {
-//   labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3","4-10", "11-17", "18-24", "25-31"],
-//   datasets:[{
-//     data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-//     backgroundColor: 'rgba(116, 119, 191, .3)',
-//     borderWidth: 1
-//     }]
-//   }
-// });
-
-
 
 // //Line Graph
 const trafficCanvas = document.getElementById("traffic-chart").getContext('2d');
@@ -155,15 +128,33 @@ const user = document.getElementById('userField');
 const message = document.getElementById('messageField');
 const send = document.getElementById('send');
 
+// send.addEventListener('click', ()=> {
+//   alert("This button works.");
+// });
+//
+function messages () {
+  if (user.value === "" && message.value === "") {
+      window.alert('Please fill out user and message fields before sending');
+    } else if (user.value === "") {
+      window.alert('Please fill out user field before sending');
+    } else if (message.vaule = "") {
+      window.alert('Please fill out message field before sending');
+    } else {
+      window.alert(`Message successfully sent to: ${user.value}`);
+    }
+}
+
+// type="button" onclick="messages()"
+
 send.addEventListener('click', ()=> {
   //ensure user and message fields are filled out
   if (user.value === "" && message.value === "") {
-    alert('Please fill out user and message fields before sending');
-  } else if (user.value === "") {
-    alert('Please fill out user field before sending');
-  } else if (message.vaule = "") {
-    alert('Please fill out message field before sending');
-  } else {
-    alert(`Message successfully sent to: ${user.value}`);
-  }
+      window.alert('Please fill out user and message fields before sending');
+    } else if (user.value === "") {
+      window.alert('Please fill out user field before sending');
+    } else if (message.vaule = "") {
+      window.alert('Please fill out message field before sending');
+    } else {
+      window.alert(`Message successfully sent to: ${user.value}`);
+    }
 });
